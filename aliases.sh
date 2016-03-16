e34bd2b82b794323bc6e9d50ff201537c42f59c0
G43riko <gcsollei@hotmail.com>
2016-03-16 21:31:07 +0100

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -18,3 +18,25 @@ alias scanPort='nmap -v -sT'
 alias pingg="ping 8.8.8.8"
 alias sem="cd $semester"
 alias extIp="wget http://ipinfo.io/ip -qO -"
+
+## pass options to free ## 
+alias meminfo='free -m -l -t'
+ 
+## get top process eating memory
+alias psmem='ps auxf | sort -nr -k 4'
+alias psmem10='ps auxf | sort -nr -k 4 | head -10'
+ 
+## get top process eating cpu ##
+alias pscpu='ps auxf | sort -nr -k 3'
+alias pscpu10='ps auxf | sort -nr -k 3 | head -10'
+
+## Get server cpu info ##
+alias cpuinfo='lscpu'
+
+## get GPU ram on desktop / laptop## 
+alias gpumeminfo='grep -i --color memory /var/log/Xorg.0.log'
+
+alias h="history"
+alias s="service --staus-all"
+alias j="jobs -a"
+alias onStart="nano ~/.myconf/onTerminalRun"
