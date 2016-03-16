1280fe2249119db004ebaa4762659695ee5cf10d
G43riko <gcsollei@hotmail.com>
2016-03-16 19:01:16 +0100

    Create functions.sh

---
+++ functions.sh
@@ -0,0 +1,32 @@
+clean_all(){
+    sudo apt-get autoremove
+    sudo apt-get autoclean
+    #sudo apt-get clean
+    sudo find ~ -type f \( -name '*.swp' -o -name '*~' -o -name '*.bak' -o $
+}
+
+update_all(){
+    sudo apt-get update
+    sudo apt-get upgrade
+}
+
+extract () {
+    if [ -f $1 ] ; then
+        case $1 in
+            *.tar.bz2)   tar xvjf $1    ;;
+            *.tar.gz)    tar xvzf $1    ;;
+            *.bz2)       bunzip2 $1     ;;
+            *.rar)       unrar x $1     ;;
+            *.gz)        gunzip $1      ;;
+            *.tar)       tar xvf $1     ;;
+            *.tbz2)      tar xvjf $1    ;;
+            *.tgz)       tar xvzf $1    ;;
+            *.zip)       unzip $1       ;;
+            *.Z)         uncompress $1  ;;
+            *.7z)        7z x $1        ;;
+            *)           echo "don't know how to extract '$1'..." ;;
+        esac
+    else
+        echo "'$1' is not a valid file!"
+    fi
+}
