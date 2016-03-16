6247c3e67470478dc6949ce228596c73b2b89242
G43riko <gcsollei@hotmail.com>
2016-03-16 18:59:36 +0100

    Create aliases.sh

---
+++ aliases.sh
@@ -0,0 +1,19 @@
+alias raspberryExt='ssh -p 1236 pi@`wget http://ipinfo.io/ip -qO -`'
+alias semester='/home/gabriel/Dropbox/Semester\ 5/';
+alias scanLan='sudo arp-scan -l --interface=wlan0'
+alias sublime='/opt/sublime_text/sublime_text '
+alias chrome='/usr/bin/google-chrome-stable'
+alias osa='ssh csollei13@osa.fiit.stuba.sk'
+alias battery="sudo powertop --auto-tune"
+alias db="mysql -u root -h localhost -p"
+alias agr='sudo apt-get --purge remove'
+alias raspberry='ssh pi@192.168.0.139'
+alias listenLan='tcpdump -i wlan0'
+alias agi='sudo apt-get install'
+alias killjava="killall -9 java"
+alias agu='sudo apt-get update'
+alias pt='/opt/pt/packettracer'
+alias acs='apt-cache search'
+alias scanPort='nmap -v -sT'
+alias pingg="ping 8.8.8.8"
+alias sem="cd $semester"
