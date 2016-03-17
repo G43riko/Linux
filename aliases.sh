a7d9f7a44d83083ce2c4449ad9947a5ac9824439
G43riko <gcsollei@hotmail.com>
2016-03-17 13:25:30 +0100

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -3,6 +3,7 @@ alias semester='/home/gabriel/Dropbox/Semester\ 5/';
 alias scanLan='sudo arp-scan -l --interface=wlan0'
 alias sublime='/opt/sublime_text/sublime_text '
 alias chrome='/usr/bin/google-chrome-stable'
+alias extIp="wget http://ipinfo.io/ip -qO -"
 alias osa='ssh csollei13@osa.fiit.stuba.sk'
 alias battery="sudo powertop --auto-tune"
 alias db="mysql -u root -h localhost -p"
@@ -11,13 +12,13 @@ alias raspberry='ssh pi@192.168.0.139'
 alias listenLan='tcpdump -i wlan0'
 alias agi='sudo apt-get install'
 alias killjava="killall -9 java"
+alias cmount="mount | column -t"
 alias agu='sudo apt-get update'
 alias pt='/opt/pt/packettracer'
 alias acs='apt-cache search'
 alias scanPort='nmap -v -sT'
 alias pingg="ping 8.8.8.8"
 alias sem="cd $semester"
-alias extIp="wget http://ipinfo.io/ip -qO -"
 
 ## pass options to free ## 
 alias meminfo='free -m -l -t'
@@ -40,3 +41,13 @@ alias h="history"
 alias s="service --staus-all"
 alias j="jobs -a"
 alias onStart="nano ~/.myconf/onTerminalRun"
+
+#Displays the current sound volume
+alias volume="amixer get Master | sed '1,4 d' | cut -d [ -f 2 | cut -d ] -f 1"
+
+#Show which applications are connecting to the network
+alias listen="lsof -P -i -n" 
+
+#Show the active ports
+alias port='netstat -tulanp'
+
