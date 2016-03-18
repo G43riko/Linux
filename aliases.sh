c726183caa036e85179906d7f799eb96bbc06718
G43riko <gcsollei@hotmail.com>
2016-03-18 09:32:33 +0100

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -1,25 +1,53 @@
+##########################
+########SPECIFIED#########
+##########################
+
+alias pgStart="sudo -u postgres /usr/lib/postgresql/9.4/bin/postgres -D /home/pi/DB/ > /tmp/pg_logfile 2>&1 &"
 alias raspberryExt='ssh -p 1236 pi@`wget http://ipinfo.io/ip -qO -`'
 alias semester='/home/gabriel/Dropbox/Semester\ 5/';
-alias scanLan='sudo arp-scan -l --interface=wlan0'
 alias sublime='/opt/sublime_text/sublime_text '
 alias chrome='/usr/bin/google-chrome-stable'
-alias extIp="wget http://ipinfo.io/ip -qO -"
+alias onStart="nano ~/.myconf/onTerminalRun"
 alias osa='ssh csollei13@osa.fiit.stuba.sk'
-alias battery="sudo powertop --auto-tune"
 alias db="mysql -u root -h localhost -p"
-alias agr='sudo apt-get --purge remove'
 alias raspberry='ssh pi@192.168.0.139'
-alias listenLan='tcpdump -i wlan0'
-alias agi='sudo apt-get install'
 alias killjava="killall -9 java"
-alias cmount="mount | column -t"
-alias agu='sudo apt-get update'
 alias pt='/opt/pt/packettracer'
-alias acs='apt-cache search'
-alias scanPort='nmap -v -sT'
-alias pingg="ping 8.8.8.8"
 alias sem="cd $semester"
-alias pgStart="sudo -u postgres /usr/lib/postgresql/9.4/bin/postgres -D /home/pi/DB/ > /tmp/pg_logfile 2>&1 &"
+
+##########################
+########UNIVERSAL#########
+##########################
+
+alias h="history"
+alias s="service --staus-all"
+alias j="jobs -a"
+
+alias agr='sudo apt-get --purge remove'
+alias agi='sudo apt-get install'
+alias agu='sudo apt-get update'
+alias acs='apt-cache search'
+
+#vypíše pripojené zariadenia v sieti
+alias scanLan='sudo arp-scan -l --interface=wlan0'
+
+#vypíše externú IP
+alias extIp="wget http://ipinfo.io/ip -qO -"
+
+#zapne šetrenie batérie
+alias battery="sudo powertop --auto-tune"
+
+#počúva dáta v sieti
+alias listenLan='tcpdump -i wlan0'
+
+#vypíše pripojené zariadenie
+alias cmount="mount | column -t"
+
+#preskenuje a vypíše otvorené porty na danom zariadení
+alias scanPort='nmap -v -sT'
+
+#ping google
+alias pingg="ping 8.8.8.8"
 
 ## pass options to free ## 
 alias meminfo='free -m -l -t'
@@ -37,11 +65,6 @@ alias cpuinfo='lscpu'
 
 ## get GPU ram on desktop / laptop## 
 alias gpumeminfo='grep -i --color memory /var/log/Xorg.0.log'
-
-alias h="history"
-alias s="service --staus-all"
-alias j="jobs -a"
-alias onStart="nano ~/.myconf/onTerminalRun"
 
 #Displays the current sound volume
 alias volume="amixer get Master | sed '1,4 d' | cut -d [ -f 2 | cut -d ] -f 1"
